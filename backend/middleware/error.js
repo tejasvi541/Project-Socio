ed0107ed82d4a3d3e219036b545ea3ff49a5f281
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-05-19 18:27:05 +0530

    Important middlewares and Database setup

---
+++ backend/middleware/error.js
@@ -0,0 +1,34 @@
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+  // Log to console for the developer
+  console.log(err);
+
+  // Mongoose bad object id
+  if (err.name === "CastError") {
+    const message = `Resource not found `;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate key error
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation errors
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+  });
+};
+
+module.exports = errorHandler;
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
 // Loading Env Vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to Database
+connectDB();
 
 // Initialising Express Constructor
 const app = express();
@@ -16,6 +23,15 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+// Development logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Error handler middleware (Should be after mounting routes as otherwise it will not be able to
+// errors otherwise)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
